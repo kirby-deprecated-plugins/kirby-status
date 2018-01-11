e8b81149f1c3d0419c76ace8e4cb19059badc0c4
Jens Törnell <jens.tornell@gmail.com>
2018-01-11 07:51:16 +0100

    Missing root file fixed

---
+++ kirby-status.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . DS . 'page-methods.php';
+require_once __DIR__ . DS . 'pages-methods.php';
+require_once __DIR__ . DS . 'controller-helper.php';
+
+// Register field
+$kirby->set('field', 'status',  __DIR__ . DS . 'field');
